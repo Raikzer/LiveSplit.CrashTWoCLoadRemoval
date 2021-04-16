006218da1c89464504aac2530c248954f0b528ab
SHTDJ <36552562+SHTDJ@users.noreply.github.com>
2021-04-16 18:53:01 +0200

    adjust for base xbox color desaturation

--- FeatureDetector.cs
+++ FeatureDetector.cs
@@ -30,7 +30,7 @@ namespace CrashTWoCLoadDetector
             // Copy the RGB values into the array.
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
             int BValue = 80 + blacklevel;
-            int GValue = 110 + blacklevel;
+            int GValue = 110 - blacklevel;
             for (int i = 0; i < bytes - 3; i += 4)
             {
 
@@ -84,8 +84,8 @@ namespace CrashTWoCLoadDetector
             // Copy the RGB values into the array.
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
             int BValue = 65 + blacklevel;
-            int GValue = 110 + blacklevel;
-            int RValue = 190 + blacklevel;
+            int GValue = 110 - blacklevel;
+            int RValue = 190 - blacklevel;
             for (int i = 0; i < bytes - 3; i += 4)
             {
 
@@ -153,6 +153,11 @@ namespace CrashTWoCLoadDetector
             return b == g && b == r;
         }
 
+        private static bool IsSimiliarColor(byte b, byte g, byte r)
+        {
+            return Math.Abs(b - g + b - r) < 10;
+        }
+
         public static int GetBlackLevel(ref Bitmap capture)
         {
             Rectangle rect = new Rectangle(0, 0, capture.Width, capture.Height);
@@ -173,7 +178,8 @@ namespace CrashTWoCLoadDetector
             int jitter_max = (int)(bytes * 0.01);
             for (int i = bytes - 2; i >= 0; i -= 4)
             {
-                if (!IsGreyscale(rgbValues[i], rgbValues[i - 1], rgbValues[i - 2]))
+                if (!IsGreyscale(rgbValues[i], rgbValues[i - 1], rgbValues[i - 2]) &&
+                    !IsSimiliarColor(rgbValues[i], rgbValues[i - 1], rgbValues[i - 2]))
                 {
                     jitter++;
                     if (jitter > jitter_max)
@@ -182,9 +188,20 @@ namespace CrashTWoCLoadDetector
                         return -1;
                     }
                 }
-                else if (rgbValues[i] > blacklevel)
+                else
                 {
-                    blacklevel = rgbValues[i];
+                    if (rgbValues[i] > blacklevel)
+                    {
+                        blacklevel = rgbValues[i];
+                    }
+                    if (rgbValues[i - 1] > blacklevel)
+                    {
+                        blacklevel = rgbValues[i - 1];
+                    }
+                    if (rgbValues[i - 2] > blacklevel)
+                    {
+                        blacklevel = rgbValues[i - 2];
+                    }
                 }
             }
             capture.UnlockBits(captureData);
